4e8179bd86a362c5119895c26a6c43339bf13bf6
DefinitelyNotAbhishek <110118289+DefinitelyNotAbhishek@users.noreply.github.com>
2024-03-04 15:18:52 +0000

    Learnt Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const variable_1 = "abhishek h boss";
+
+const myName = "Abhishek Bharti"
+let myAge = 22
+var myHobby = "Watching Movie"
+myFavColor = "Dark Green"
+
+// myName = "DefinitelyNotAbhishek"
+// we cannot assign another value to a variable which was created using Constt Keyword
+
+yAge = 23;
+myHobby = "Trolling"
+myFavColor = "Purple"
+
+
+/*
+we do not use Var anymore bcoz Var is having scope problem which is if i declare var at one particular chunk of code it gets reserved for entire 
+code snippet hence in modern days Let keyword is used more in comparison and is recommended too 
+*/
+
+let myDream;
+
+
+// console.table([myAge,myHobby,myFavColor])
+
+// console.log(myName)
+console.table([myName,myHobby,myAge,myFavColor,myDream])
